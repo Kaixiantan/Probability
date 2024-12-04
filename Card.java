0eacbdc7baa9f317915b289214d05c03880190b0
CallineKelly <167273812+CallineKelly@users.noreply.github.com>
2024-12-04 19:12:40 +0800

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,36 @@
+package probability.project;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Neoh
+ * get suit and rank from Deck class
+ */
+public class Card {
+    private String suit;
+    private String rank;
+    
+    public Card(String suit, String rank){
+        this.suit = suit;
+        this.rank = rank;
+    }
+    
+    public String getSuit(){
+        return suit;
+    }
+    
+    public String getRank(){
+        return rank;
+    }
+    
+    //help to return string like "Ace of Hearts", "2 of Hearts"
+    @Override
+    public String toString(){
+        return rank + " of " +suit;
+    }
+}
